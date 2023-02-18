8294a6d83d39b334ec58863847c91169f70aaeeb
Lucas <lucsbasto@gmail.com>
2023-02-18 16:07:10 -0300

    feat: add graduate entity

--- src/graduate/entities/graduate.entity.ts
+++ src/graduate/entities/graduate.entity.ts
@@ -1,10 +1,12 @@
+import { Field } from "@nestjs/graphql";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Graduate {
+  @Field()
   @PrimaryGeneratedColumn()
   id: number
 
   @Column()
-  name: string;
+  name: string
 }
