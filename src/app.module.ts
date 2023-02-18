d535a3be944770f9870a6e492a2bcaeaedbd2a9c
Lucas <lucsbasto@gmail.com>
2023-02-18 15:57:17 -0300

    feat: add graphql config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraduateModule } from './graduate/graduate.module';
 import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
     GraduateModule
 ],
--- src/graduate/graduate.module.ts
+++ src/graduate/graduate.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GraduateController } from './graduate.controller';
 import { GraduateService } from './graduate.service';
+import { GraduateResolver } from './graduate.resolver';
 
 @Module({
   controllers: [GraduateController],
-  providers: [GraduateService]
+  providers: [GraduateService, GraduateResolver]
 })
 export class GraduateModule {}
